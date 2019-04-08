651f9a77bf7eb38aa53eaa1f28a56c95e3e3a480
brandon corey <bcorey85@gmail.com>
2019-04-08 15:59:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
+<<<<<<< HEAD
   if (x % 3 == 0 && x % 5 == 0)
    "FizzBuzz"
   elsif x % 5 == 0
@@ -11,4 +12,14 @@ def fizzbuzz(x)
   else
    nil
   end
+=======
+  if x % 3 == 0
+    puts "Fizz"
+  elsif x % == 5
+    puts "Buzz"
+  elsif x % == 15
+    puts "FizzBuzz"
+  else
+    puts "nil"
+>>>>>>> 548658928e4b73b791f5b7a211fc07f329fd9e06
 end
